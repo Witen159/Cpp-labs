9c44dcd2dce1c1b600afaf8fa3c99be7aa258ddb
Witen <44116296+Witen159@users.noreply.github.com>
2021-03-30 14:30:02 +0300

    Add files via upload

--- Homework2/polynomial.h
+++ Homework2/polynomial.h
@@ -12,53 +12,55 @@ class Polynomial {
 public:
     Polynomial();
 
-    Polynomial(int first, int second, const int *mas);
+    Polynomial(int first, int second, const int* mas);
 
-    Polynomial(const Polynomial &pol);
+    Polynomial(const Polynomial& pol);
 
-    Polynomial &operator=(const Polynomial &pol);
+    Polynomial& operator=(const Polynomial& pol);
 
-    friend ostream &operator<<(ostream &out, const Polynomial &pol);
+    friend ostream& operator<<(ostream& out, const Polynomial& pol);
 
-    friend bool operator==(const Polynomial &left, const Polynomial &right);
+    friend bool operator==(const Polynomial& left, const Polynomial& right);
 
-    friend bool operator!=(const Polynomial &left, const Polynomial &right);
+    friend bool operator!=(const Polynomial& left, const Polynomial& right);
 
-    friend Polynomial operator+(const Polynomial &left, const Polynomial &right);
+    friend Polynomial operator+(const Polynomial& left, const Polynomial& right);
 
-    friend Polynomial operator-(const Polynomial &left, const Polynomial &right);
+    friend Polynomial operator-(const Polynomial& left, Polynomial& right);
 
-    friend Polynomial operator-(const Polynomial &pol);
+    friend Polynomial operator-(const Polynomial& pol);
 
-    Polynomial &operator+=(const Polynomial &pol);
+    Polynomial& operator+=(const Polynomial& pol);
 
-    Polynomial &operator-=(const Polynomial &pol);
+    Polynomial& operator-=(Polynomial& pol);
 
-    Polynomial &operator*=(const Polynomial &pol);
+    Polynomial& operator*=(const Polynomial& pol);
 
-    Polynomial &operator*=(int num);
+    Polynomial& operator*=(int num);
 
-    Polynomial &operator/=(int num);
+    Polynomial& operator/=(int num);
 
-    friend Polynomial operator*(const Polynomial &left, const Polynomial &right);
+    friend Polynomial operator*(const Polynomial& left, const Polynomial& right);
 
-    friend Polynomial operator*(const Polynomial &pol, int num);
+    friend Polynomial operator*(const Polynomial& pol, int num);
 
-    friend Polynomial operator*(int num, const Polynomial &pol);
+    friend Polynomial operator*(int num, const Polynomial& pol);
 
-    friend Polynomial operator/(const Polynomial &pol, int num);
+    friend Polynomial operator/(const Polynomial& pol, int num);
 
     int operator[](int num) const;
 
-    int &operator[](int num);
+    int& operator[](int num);
 
     double get(int num);
 
     ~Polynomial();
 
+    void minus();
+
 private:
     int min_pow;
     int max_pow;
-    int *constants;
+    int* constants;
     int n;
 };
